b04d9e72cee00146ca96a4df1c8f2c8cec9336f2
Hackkod <103487636+Hackkod@users.noreply.github.com>
2024-06-21 11:20:30 +0300

    feat: создал HardSkillApp, модель, сериализатор, представление

---
+++ backend/hardskillApp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import HardSkill
+from .serializers import HardSkillSerializer
+
+
+class HardSkillListView(viewsets.ModelViewSet):
+    queryset = HardSkill.objects.all()
+    serializer_class = HardSkillSerializer
