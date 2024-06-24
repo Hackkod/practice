d7a954167796e1ed08d3c460eb0ef5af091b2996
Hackkod <103487636+Hackkod@users.noreply.github.com>
2024-06-24 13:09:46 +0300

    feat: добавил валидацию для начала и конца обучения, курс теперь вычисляется автоматически

--- backend/anketApp/models.py
+++ backend/anketApp/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator, MaxValueValidator
+import datetime
+
 from utils.models import FormBase
 from hardskillApp.models import HardSkill
 
@@ -6,9 +10,27 @@ from hardskillApp.models import HardSkill
 class StudentForm(FormBase):
     hard_skills_id = models.ManyToManyField(HardSkill)
     establishment = models.CharField(max_length=1024)
-    start_study_date = models.DateField()
-    end_study_date = models.DateField()
+    start_study_year = models.IntegerField(
+        validators=[MinValueValidator(2000), MaxValueValidator(2100)],
+        default=None)
+    end_study_year = models.IntegerField(
+        validators=[MinValueValidator(2000), MaxValueValidator(2100)],
+        default=None)
     course = models.IntegerField()
+
+    def save(self, *args, **kwargs):
+        start_year = self.start_study_year
+        end_year = self.end_study_year
+        if start_year and end_year and (end_year - start_year) < 4:
+            raise ValidationError('Срок обучения должен составлять не менее 4-х лет')
+        curr_date = datetime.date.today()
+        if start_year > curr_date.year or (start_year == curr_date.year and curr_date.month < 9):
+            raise ValidationError('Старт обучения не может быть в будущем')
+        if curr_date.month < 9:
+            self.course = curr_date.year - start_year
+        else:
+            self.course = curr_date.year - start_year + 1
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.surname} {self.name} {self.patronymic}'
