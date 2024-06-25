0ad5fb44fdfde30d0371129ad7e6aeff008ec03c
Igor <ex@mail.ru>
2024-06-25 09:01:14 +0300

    feat: рефакторинг базовых моделей

--- backend/utils/models.py
+++ backend/utils/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 
 class TimeStampedModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        null=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        null=True
+    )
 
     class Meta:
         abstract = True
@@ -15,23 +21,40 @@ class AnketBase(TimeStampedModel):
         ('F', 'Female')
     ]
 
-    surname = models.CharField(max_length=1024)
-    name = models.CharField(max_length=1024)
-    patronymic = models.CharField(max_length=1024)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    surname = models.CharField(
+        max_length=1024
+    )
+    name = models.CharField(
+        max_length=1024
+    )
+    patronymic = models.CharField(
+        max_length=1024
+    )
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDER_CHOICES
+    )
     birth_date = models.DateField()
-    soft_skills = models.TextField(blank=True)
-    other_info = models.TextField(blank=True)
+    soft_skills = models.TextField(
+        blank=True
+    )
+    other_info = models.TextField(
+        blank=True
+    )
 
     class Meta:
         abstract = True
 
 
 class EventBase(TimeStampedModel):
-    name = models.CharField(max_length=1024)
+    name = models.CharField(
+        max_length=1024
+    )
     start_date = models.DateField()
     end_date = models.DateField()
-    description = models.TextField(blank=True)
+    description = models.TextField(
+        blank=True
+    )
 
     class Meta:
         abstract = True
