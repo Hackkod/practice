12904eee732c8bc957e7b9a6120a064341e13da9
Igor <ex@mail.ru>
2024-07-10 10:46:33 +0300

    feat: реализоал возможность выбора всех хард скиллов

--- frontend/src/components/StudentForm.vue
+++ frontend/src/components/StudentForm.vue
@@ -183,6 +183,17 @@
               (+{{ form.hard_skills_id.length - 1 }})
             </span>
           </template>
+          <template #prepend-item>
+            <v-list-item title="Выбрать все" @click="toggle">
+              <template #prepend>
+                <v-checkbox-btn
+                  :indeterminate="SomeHardSkill && !AllHardSkill"
+                  :model-value="AllHardSkill"
+                ></v-checkbox-btn>
+              </template>
+            </v-list-item>
+            <v-divider class="mt-2"></v-divider>
+          </template>
           <template #append>
             <span class="append-icon">
               <v-icon
@@ -305,6 +316,12 @@ export default {
       }
       return "";
     },
+    SomeHardSkill() {
+      return this.hard_skill_ids.length > 0;
+    },
+    AllHardSkill() {
+      return this.hard_skill_ids.length === this.form.hard_skills_id.length;
+    },
   },
   methods: {
     secondColumnClass(readonly) {
@@ -372,6 +389,13 @@ export default {
         this.form.hard_skills_id = [];
       } catch (e) {
         alert("Ошибка при удалении хард скиллов");
+      }
+    },
+    toggle() {
+      if (this.AllHardSkill) {
+        this.form.hard_skills_id = [];
+      } else {
+        this.form.hard_skills_id = this.hard_skill_ids.map((skill) => skill.id);
       }
     },
   },
